3b746d8327ce59d7eb01e1443f286f53a119c29f
Vizeu <leovizeu@hotmail.com>
2024-01-29 09:06:53 -0300

    fixed some minor errors

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['cdn.imagin.studio']
+  },
+  typescript: {
+    ignoreBuildErrors: true
+  }
+  
+}
 
 module.exports = nextConfig
--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,6 @@
 import { CarProps } from "@/types";
 
-export async function fetchCars() {
+ export async function fetchCars() {
     const headers = {
         'X-RapidAPI-Key': '0df816d264msh45659df64ebec16p1f2b11jsn8f612039af4c',
         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
@@ -31,7 +31,59 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 
     return totalPricePerDay;
 }
+/*
+const model: string = 'camry';
+const apiKey: string = 'JI1r83PFwe5jFgAkrFgofQ==sfUEO5YE672r2mmJ';
+const url = `https://api.api-ninjas.com/v1/cars?model=corolla`;
 
-//export const generateCarImageURL (car: CarProps, angle?: string) => {
+fetch(url, {
+  method: 'GET',
+  headers: {
+    'X-Api-Key': apiKey,
+  },
+})
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error('Request failed:', error.message);
+  });
+
+
+export async function fetchCars() {
+  const headers = {
+    "X-Api-Key": apiKey,
+  };
+
+  const response = await fetch(
+    `https://api.api-ninjas.com/v1/cars?model=${model}`,
+    {
+      headers: headers,
+    }
+  );
+
+  const result = await response.json();
+
+  return result;
+}
+
+ export const generateCarImageURL = (car: CarProps, angle?: string) => {
     
-//}
+    const url = new URL ('https://cnd.imagin.studio/getImage');
+
+    const { make, year, model } = car;
+    
+    url.searchParams.append('customer', 'hrjavascript-mastery');
+    url.searchParams.append('modelFamily', model.split('')[0]);
+    url.searchParams.append('zoomType', 'fullscreen');
+    url.searchParams.append('modelYear', '${year}');
+    url.searchParams.append('angle', '${angle}');
+    
+    return '${url}';
+} */
