06625b366ea51020111465e65d9f213b11c6c378
Vizeu <leovizeu@hotmail.com>
2024-01-30 19:04:36 -0300

    making the search function

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,16 +2,51 @@
 
 import { useState } from "react"
 import { SearchManufacturer } from "./"
+import Image from 'next/image';
+
+const SearchButton = ({ otherClasses }: {otherClasses: string }) => (
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+  <Image
+    src="/magnifying-glass.svg"
+    alt="magnifying glass"
+    width={40}
+    height={40}
+    className="object-contain"
+  />
+  </button>
+)
 
 const SearchBar = () => {
-    const [manufacturer, setManufacturer] = useState('')
+    const [manufacturer, setManufacturer] = useState('');
+    const [model, setModel] = useState('');
 
     const handleSearch = () => {}
 
   return (
     <form className='searchbar' onSubmit={handleSearch}>
         <div className="searchbar__item">
-            <SearchManufacturer manufacturer={manufacturer} setManufacturer={setManufacturer}/>
+            <SearchManufacturer 
+              manufacturer={manufacturer} 
+              setManufacturer={setManufacturer}
+            />
+        <SearchButton otherClasses= "sm:hidden" />
+        </div>
+        <div className="searchbar__item">
+          <Image 
+            src="/model-icon.png"
+            width={25}
+            height={25}
+            className="absolute w-[20] h-[20] ml-4"
+            alt="Car Model"
+          />
+          <input 
+          type="text"
+          name="model"
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder="Tiguan"
+          className="searchbar__input"
+          />
         </div>
     </form>
   )
